f635511392cc2975006fd34d40ee7018eb115e5e
daurarapp-tkp <daura.rapp@gmail.com>
2026-09-23 20:46:03 +0545

    Add Next.js image configuration

---
+++ next.config.mjs
@@ -0,0 +1,3 @@
+/** @type {import('next').NextConfig} */
+const nextConfig={images:{remotePatterns:[{protocol:'https',hostname:'images.unsplash.com'},{protocol:'https',hostname:'xcmyieefdpaaipngphym.supabase.co'}]}};
+export default nextConfig;
